9cabcc2cdfa1978be4f3cb2b59fd9d4f0ca7e36d
_BrenD_ <35214019+brend32@users.noreply.github.com>
2024-12-03 19:37:43 +0200

    Snake body

--- Assets/Scripts/SnakeController.cs
+++ Assets/Scripts/SnakeController.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEditor;
 using UnityEngine;
 
@@ -12,16 +14,38 @@ public enum Direction
 
 public class SnakeController : MonoBehaviour
 {
+	public Transform BodyCellPrefab;
 	public Room Room;
 	public Vector2Int RoomPosition;
 	public Direction MoveDirection;
 	public float MoveDelay = 1.5f;
 
 	private float _timer;
+	private List<Transform> _body = new();
 
 	public void Start()
 	{
 		TeleportTo(RoomPosition);
+		AddInitialBodyCells();
+	}
+
+	public void AddInitialBodyCells()
+	{
+		Vector2Int startPosition = RoomPosition;
+		for (int i = 0; i < 3; i++)
+		{
+			startPosition.x--;
+			Vector2 spawnPoint = Room.GetCellPosition(startPosition.x, startPosition.y);
+			Transform cell = SpawnBodyCell();
+
+			cell.localPosition = spawnPoint;
+			_body.Add(cell);
+		}
+	} 
+
+	private Transform SpawnBodyCell()
+	{
+		return Instantiate(BodyCellPrefab, transform.parent);
 	}
 
 	public void TeleportTo(Vector2Int roomPosition)
@@ -46,13 +70,31 @@ public class SnakeController : MonoBehaviour
 	{
 		Vector2Int direction = GetForwardVector();
 
-		Vector2Int newPosition = RoomPosition + direction;
+		Vector2Int oldPosition = RoomPosition;
+		Vector2Int newPosition = oldPosition + direction;
 		
 		//TODO: Collision check
 		
+		Grow();
+		MoveBody(oldPosition);
 		RoomPosition = newPosition;
 		TeleportTo(RoomPosition);
 		FaceDirection(direction);
+	}
+
+	private void Grow()
+	{
+		Transform cell = SpawnBodyCell();
+		_body.Add(cell);
+	}
+
+	private void MoveBody(Vector2Int oldHeadPosition)
+	{
+		Transform lastCell = _body.Last();
+		lastCell.localPosition = Room.GetCellPosition(oldHeadPosition.x, oldHeadPosition.y);
+
+		_body.Remove(lastCell);
+		_body.Insert(0, lastCell);
 	}
 
 	public void FaceDirection(Vector2Int direction)
